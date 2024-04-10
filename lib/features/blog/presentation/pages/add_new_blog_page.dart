255907035cc84540be3eabc5112bf9e34886a267
Sushil Kumar <72073576+voltzylex@users.noreply.github.com>
2024-04-10 17:55:30 +0530

    created rounded container with selection of image

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,7 +1,10 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddNewBlogPage extends StatefulWidget {
-  static route() => MaterialPageRoute(builder: (context) => const AddNewBlogPage());
+  static route() =>
+      MaterialPageRoute(builder: (context) => const AddNewBlogPage());
   const AddNewBlogPage({super.key});
 
   @override
@@ -19,6 +22,36 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
             icon: const Icon(Icons.done_rounded),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            DottedBorder(
+              color: AppPallete.borderColor,
+              dashPattern: const [10, 4],
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              strokeCap: StrokeCap.round,
+              child: const SizedBox(
+                  height: 150,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.folder_open_rounded,
+                        size: 50,
+                      ),
+                      Text(
+                        'Select Your Image',
+                        style: TextStyle(),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }

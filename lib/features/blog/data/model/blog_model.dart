c317a919985a3328c248929d46bea54d68f6ac30
sushil <baymax732001@gmail.com>
2024-04-13 19:58:54 +0530

    created blog model

---
+++ lib/features/blog/data/model/blog_model.dart
@@ -0,0 +1,13 @@
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+
+class BlogModel extends Blog {
+  BlogModel({
+    required super.id,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.posterId,
+    required super.updatedAt,
+    required super.topics,
+  });
+}
